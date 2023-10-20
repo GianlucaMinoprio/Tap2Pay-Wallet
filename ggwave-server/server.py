53c1b9fc1e0e1ce126eafab0bcaaa07223fdc5b2
GianlucaMinoprio <58565032+GianlucaMinoprio@users.noreply.github.com>
2023-10-20 13:17:55 +0200

    Python server to handle ultrasound with .wav file and test

--- ggwave-server/server.py
+++ ggwave-server/server.py
@@ -1,59 +1,69 @@
-from flask import Flask, request, send_file, jsonify
-from pydub import AudioSegment
-import numpy as np
+from flask import Flask, request, jsonify, send_file
 import ggwave
+import soundfile as sf
+import numpy as np
+import io
 import tempfile
-import os
 
 app = Flask(__name__)
 
-
-def waveform_to_audio_file(waveform, filename):
-    audio_segment = AudioSegment(
-        waveform,
-        frame_rate=48000,
-        sample_width=2,
-        channels=1
-    )
-    audio_segment.export(filename, format="wav")
+# Initialize ggwave instance once at app startup
+ggwave_instance = ggwave.init()
 
 
 @app.route('/encode', methods=['POST'])
 def encode():
-    text = request.form.get('text')
-    if not text:
-        return "No text provided", 400
-
-    waveform = ggwave.encode(text, protocolId=5, volume=10)
-    encoded_audio_file_name = tempfile.mktemp(suffix='.wav')
-    waveform_to_audio_file(np.frombuffer(
-        waveform, dtype=np.float32), encoded_audio_file_name)
-
-    return send_file(encoded_audio_file_name, as_attachment=True, attachment_filename='encoded_audio.wav')
+    try:
+        text = request.form['text']
+        print(f"Encoding text: {text}")  # Debug print
+        waveform = ggwave.encode(text, protocolId=5, volume=10)
+        waveform_bytes = bytes(waveform)
+        waveform_np = np.frombuffer(waveform_bytes, dtype=np.float32)
+        with tempfile.NamedTemporaryFile(suffix='.wav', delete=True) as tmpfile:
+            sf.write(tmpfile.name, waveform_np, samplerate=48000)
+            print(f"Saved encoded audio as '{tmpfile.name}'")  # Debug print
+            return send_file(tmpfile.name, as_attachment=True, cache_timeout=0)
+    except Exception as e:
+        return str(e), 500
 
 
 @app.route('/decode', methods=['POST'])
 def decode():
     try:
-        audio_file = request.files['audio_file']
-        audio_segment = AudioSegment.from_file(audio_file)
-        waveform = np.array(audio_segment.get_array_of_samples())
-        if audio_segment.channels == 2:
-            waveform = waveform.reshape((-1, 2)).mean(axis=1)
-        # Convert to unsigned 8-bit integer values
-        waveform = (waveform / 32767.0 * 255).astype(np.uint8)
-        waveform_bytes = waveform.tobytes()  # Now convert the integer values to bytes
-        print(len(waveform_bytes))
-        instance = ggwave.init()
-        text = ggwave.decode(instance, waveform_bytes)
-        print(text)
-        if text is None:
-            raise ValueError("Failed to decode audio")
-        return text
+        # Access the uploaded file
+        uploaded_file = request.files['file']
+        print(f"Decoding file: {uploaded_file.filename}")  # Debug print
 
+        # Create an in-memory binary stream from the uploaded file
+        file_stream = io.BytesIO(uploaded_file.read())
+
+        decoded_text = decode_wav_file(file_stream, ggwave_instance)
+
+        if decoded_text:
+            return jsonify({"text": decoded_text})
+        else:
+            return "Failed to decode", 400
     except Exception as e:
         print(f'Error: {e}')  # Debugging line
-        return jsonify({'error': str(e)}), 500
+        return str(e), 500
+
+
+def decode_wav_file(file_stream, ggwave_instance):
+    # Open the WAV file from in-memory binary stream
+    with sf.SoundFile(file_stream, 'r') as f:
+        # Iterate through the file in chunks of 1024 frames
+        for block in f.blocks(blocksize=1024, dtype='float32'):
+            # Convert the chunk to bytes
+            data = block.tobytes()
+            res = ggwave.decode(ggwave_instance, data)
+            if res is not None:
+                try:
+                    decoded_text = res.decode("utf-8")
+                    print('Received text:', decoded_text)
+                    return decoded_text
+                except Exception as e:
+                    print("Decoding error:", e)
+    return None
 
 
 if __name__ == '__main__':
--- ggwave-server/test.py
+++ ggwave-server/test.py
@@ -1,8 +1,8 @@
 import requests
-import os
+import sounddevice as sd
+import soundfile as sf
 
-# Base URL of your server
-BASE_URL = 'http://192.168.1.32:8080/'
+BASE_URL = 'http://10.41.170.221:8080/'
 
 
 def test_encoding_decoding():
@@ -13,27 +13,33 @@ def test_encoding_decoding():
         print(f'Encoding failed: {response.text}')
         return
 
-    # Save the received audio file to disk
-    encoded_audio_file_name = 'encoded_audio.wav'
-    with open(encoded_audio_file_name, 'wb') as f:
+    # Save the received .wav file on the client side
+    with open('encoded.wav', 'wb') as f:
         f.write(response.content)
-    print(f'Encoded audio file saved as {encoded_audio_file_name}')
+    print(f"Saved encoded audio as 'encoded.wav'")
 
-    # Test Decoding
-    with open(encoded_audio_file_name, 'rb') as f:
-        response = requests.post(f'{BASE_URL}/decode', files={'audio_file': f})
-    # Optional: delete the encoded audio file after decoding
-    os.remove(encoded_audio_file_name)
+    # Test Decoding by sending back the saved .wav file
+    with open('encoded.wav', 'rb') as f:
+        response = requests.post(f'{BASE_URL}/decode', files={'file': f})
 
+    print(f'Response from server: {response.text}')
     if response.status_code != 200:
         print(f'Decoding failed: {response.text}')
         return
 
-    decoded_text = response.text
+    # Assuming server sends a JSON response
+    decoded_text = response.json()['text']
     if decoded_text == text:
         print('Encoding and decoding successful!')
     else:
         print(f'Decoding mismatch: Expected "{text}" but got "{decoded_text}"')
+
+    # Play back the saved .wav file
+    waveform_np, sr = sf.read('encoded.wav')
+    print(f"Transmitting text '{decoded_text}' ...")
+    sd.play(waveform_np, samplerate=sr)
+    sd.wait()  # Wait until audio is finished playing
+    print("Done")
 
 
 if __name__ == '__main__':
